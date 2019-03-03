fcdc047cc0062d23812169b10a7025810558b8c0
Elly Griffin <griffin096@connect.wcsu.edu>
2019-03-03 14:01:39 -0500

    Update addCourseMgmt.sql
    Changed the 'insertCourse' function implementation. Originally, the function assumed the two department abbreviations (i.e. CS, MAT) and associated number (i.e. 165) were sent in as separate parameters. New implementation takes input for Course Number as one whole parameter (i.e. CS/MAT165).
    
    This implementation now lines up more accurateley with the UI 'add course info' fields.

--- src/db/addCourseMgmt.sql
+++ src/db/addCourseMgmt.sql
@@ -8,47 +8,30 @@
 
 
 -- function to add a course / add a row to the course table
--- parameters: Department 1 (VARCHAR)  -- i.e "CS"
---             Department 2 (VARCHAR)  -- i.e "MAT" or NULL
---             Num (VARCHAR)  -- i.e "165"
+-- parameters: Num (VARCHAR)  -- i.e "CS/MAT165"
 --             Title (VARCHAR) -- i.e "Discrete Math"
 --             Credits (INT) -- i.e "4"
 --
 
 
-CREATE OR REPLACE FUNCTION insertCourse(Dept1 VARCHAR, Dept2 VARCHAR,
-                             Num VARCHAR, Title VARCHAR, Credits INT)
+CREATE OR REPLACE FUNCTION insertCourse(Num VARCHAR, Title VARCHAR, Credits INT)
 
 RETURN VOID AS
 $$
-DECLARE fullNum VARCHAR(11); -- i.e. "CS/MAT165"
 BEGIN 
-
-    -- test if Dept 2 is NULL
-    -- and assign value to fullNum
-    If $2 IS NULL
-        THEN 
-         -- i.e CS || 170  -> "CS170"
-            fullNum := $1 || $3;
-    ELSE
-        -- i.e CS || / || MAT || 165 -> "CS/MAT165"
-        Num := $1 || '/' || $2 || $3;
-         
-    END IF;
-
 
     -- check if course exists
     -- and throw exception if exists already
     IF EXISTS
     (
-        SELECT * FROM Course WHERE Course.Number = fullNum AND Course.Title = Title)
+        SELECT * FROM Course WHERE Course.Number = Num AND Course.Title = Title)
     )
     THEN
         RAISE EXCEPTION 'Course already exists';
     END IF;
 
     -- insert course
-    INSERT INTO Course VALUES (fullNum, Title, Credits);
+    INSERT INTO Course VALUES (Num, Title, Credits);
 
 END
 $$ 
