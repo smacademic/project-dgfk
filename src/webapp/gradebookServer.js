e13a66f3638070daf3246f1bce85d4802cb860d1
DasilvaB <bdbrunodasilva@hotmail.com>
2019-03-31 16:02:53 -0400

    Implmentation add/remove section
    - Also some formatting fixes

--- src/webapp/gradebookServer.js
+++ src/webapp/gradebookServer.js
@@ -473,19 +473,19 @@ app.get('getCourses', function(request, response){
 
    //Execute the query
    executeQuery(response, config, queryText, queryParams, function(result) {
-	   		var courses = []; //Put the rows from the query into json format
-												for (row in result.rows) {
-												    courses.push(
-												        {
-												            "Number": result.rows[row].number,
-												            "Title": result.rows[row].title,
-															"Credits": result.rows[row].credits
-												        }
-												    );
-												}
-												var jsonReturn = {
-												    "courses": courses
-												} //Send the json to the client
+      var courses = []; //Put the rows from the query into json format
+         for (row in result.rows) {
+               courses.push(
+                  {
+                     "Number": result.rows[row].number,
+                     "Title": result.rows[row].title,
+                  "Credits": result.rows[row].credits
+                  }
+               );
+         }
+         var jsonReturn = {
+               "courses": courses
+         } //Send the json to the client
        response.send(JSON.stringify({}));
    });
 });
@@ -512,8 +512,64 @@ app.get('modCourses', function(request, response){
    
    //execute the query
    executeQuery(response, config, queryText, queryParams, function(result) {
-   response.send(JSON.stringify({}));
+      response.send(JSON.stringify({}));
+   });
 });
+
+app.get('/addSection', function(request, response) {
+   //Decrypt the password received from the client.
+   //NOTE: We need to substitute superSecret with what we're actually implementing
+   var passwordText = sjcl.decrypt(superSecret, JSON.parse(request.query.password));
+
+   //Connection parameters for the Postgres client received in the request
+   var config = createConnectionParams(request.query.user, request.query.database,
+       passwordText, request.query.host, request.query.port);
+
+   //Get the params from the url
+   var term = request.query.term;
+   var course = request.query.course;
+   var capacity = request.query.capacity;
+   var num = request.query.num;
+   var CRN = request.query.CRN;
+   var schedule = request.query.schedule;
+   var location = request.query.location;
+   var start_date = request.query.start_date;
+   var end_date = request.query.end_date;
+   var midterm_date = request.query.midterm_date;
+   var instructor1 = request.query.instructor1;
+   var instructor2 = request.query.instructor2;
+   var instructor3 = request.query.instructor3;
+
+   //Set the query text
+   var queryText = 'SELECT addSection($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13);';
+   var queryParams = [term, course, capacity, num, CRN, schedule, location, start_date, end_date, midterm_date, instructor1, instructor2, instructor3];
+
+   //Execute the query
+   executeQuery(response, config, queryText, queryParams, function(result) {
+       response.send(JSON.stringify({}));
+   });
+});
+
+app.get('/removeSection', function(request, response) {
+  //Decrypt the password received from the client.
+  //NOTE: We need to substitute superSecret with what we're actually implementing
+  var passwordText = sjcl.decrypt(superSecret, JSON.parse(request.query.password));
+
+  //Connection parameters for the Postgres client received in the request
+  var config = createConnectionParams(request.query.user, request.query.database,
+      passwordText, request.query.host, request.query.port);
+
+  //Get the params from the url
+  var sectionID = request.query.sectionID;
+
+  //Set the query text
+  var queryText = 'SELECT removeSection($1);';
+  var queryParams = [sectionID];
+
+  //Execute the query
+  executeQuery(response, config, queryText, queryParams, function(result) {
+      response.send(JSON.stringify({}));
+  });
 });
 
 app.use(function(err, req, res, next){
