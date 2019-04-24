5216372c2d1f2f564aad9f4f5fd7fdef8f94d203
Elly Griffin <griffin096@connect.wcsu.edu>
2019-04-23 20:42:06 -0400

    Update addTermMgmt.sql
    Uodate 'getTerms' function to return a term's ID to be used when adding a section.

--- src/db/addTermMgmt.sql
+++ src/db/addTermMgmt.sql
@@ -3,14 +3,14 @@
 -- Team GEEKS, CS298 Spring 2019
 
 --Function to get all terms that a course if offered
--- returns the term's season (i.e. spring) and year (i.e 2019)
+-- returns the term's ID, season (i.e. spring), year (i.e 2019)
 
 CREATE OR REPLACE FUNCTION getTerms()
-RETURNS Table(outSeason VARCHAR, outYear NUMERIC(4,0)) AS
+RETURNS Table(outID INT, outSeason VARCHAR, outYear NUMERIC(4,0)) AS
 $$
 BEGIN
 RETURN QUERY
-    SELECT S.Name, T.Year
+    SELECT T.ID, S.Name, T.Year
     FROM Gradebook.Term T INNER JOIN Gradebook.Season S 
     ON (T.Season = S."Order");
 END
